83c88b2841ad75c1a9085b2d3a1e3f7b509f7ed0
MyUsernameIsJoker <47589295+MyUsernameIsJoker@users.noreply.github.com>
2020-07-29 08:09:19 +0800

    Initial commit

---
+++ src/com/joker/practice/ArrayList.java
@@ -0,0 +1,11 @@
+package com.joker.practice;
+
+import com.joker.practice.service.List;
+
+/**
+ * @Author: wzp
+ * @Date: 2020/7/29 8:00
+ */
+public class ArrayList implements List {
+
+}
---
+++ src/com/joker/practice/service/List.java
@@ -0,0 +1,8 @@
+package com.joker.practice.service;
+
+/**
+ * @Author: wzp
+ * @Date: 2020/7/29 8:01
+ */
+public interface List {
+}
